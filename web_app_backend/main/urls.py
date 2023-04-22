194ad87f4a667ed1de084a8fb1d144d6a912eba5
tentato <alepaw99@gmail.com>
2023-04-22 21:29:22 +0200

    displey-data-exercise-finished

--- web_app_backend/main/urls.py
+++ web_app_backend/main/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('main/', views.main, name='main'),
+    path('', views.main, name='main'),
+    path('members/', views.members, name='members'),
+    path('members/details/<int:id>', views.details, name='details'),
+
+    path('testing/', views.testing, name='testing'),  
 ]
